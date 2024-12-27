2a9d7b51f66c0c8b1a5c244f144acd69a03241e2
maritapl <taplashvilimariami@gmail.com>
2024-12-27 23:01:58 +0400

    Add Factorial implementation

---
+++ src/Factorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Factorial {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+
+            System.out.print("Enter a number to calculate its factorial: ");
+            int N = scanner.nextInt();
+
+
+            if (N < 0) {
+                System.out.println("Factorial is not defined for negative numbers.");
+            } else {
+
+                long factorial = 1;
+                for (int i = 1; i <= N; i++) {
+                    factorial *= i;
+                }
+
+
+                System.out.println("The factorial of " + N + " is: " + factorial);
+            }
+
+            scanner.close();
+        }
+    }
+
