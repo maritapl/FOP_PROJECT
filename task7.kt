7843cc59a054261f3f48dabfb51065f90cfd2454
Anna-Gabaidze <gabaidzeana1@gmail.com>
2024-12-31 18:40:04 +0400

    Update task7.kt

--- task7.kt
+++ task7.kt
@@ -1,13 +1,17 @@
 fun main() {
+    // Declare a number to check
     val number = 3947
+    // Variable to store the largest digit
     var largestDigit = 0
     var n = number
 
+    // Find the largest digit in the number
     while (n != 0) {
-        val digit = n % 10
-        if (digit > largestDigit) largestDigit = digit
-        n /= 10
+        val digit = n % 10 // Get the last digit
+        if (digit > largestDigit) largestDigit = digit // Update if the digit is larger
+        n /= 10 // Remove the last digit
     }
 
+    // Print the largest digit in the number
     println("The largest digit in $number is: $largestDigit")
 }
