1bee64d8b6b1e20362dcfc82e978abc4444260e6
Mariam Taplashvili <taplashvilimariami@gmail.com>
2025-01-01 20:29:07 +0400

    Update task2.kt

--- task2.kt
+++ task2.kt
@@ -1,8 +1,15 @@
 fun main() {
+    // Define the number for which we want to calculate the factorial
     val N = 5
+
+    // Initialize the variable to store the factorial result
     var factorial = 1
+
+    // Loop from 1 to N (inclusive) to calculate the factorial
     for (i in 1..N) {
-        factorial *= i
+        factorial *= i // Multiply the current value of factorial by i
     }
+
+    // Print the result
     println("Factorial of $N is: $factorial")
 }
